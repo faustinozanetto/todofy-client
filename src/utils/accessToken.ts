30a8283040342baafdccc81c3d781526f102cd35
Faustino Zanetto <faustizanetto@gmail.com>
2021-03-28 21:04:26 -0300

    Test

--- src/utils/accessToken.ts
+++ src/utils/accessToken.ts
@@ -2,8 +2,10 @@ let accessToken = '';
 
 export const setAccessToken = (s: string) => {
   accessToken = s;
+  console.log('Access token set to: ', accessToken);
 };
 
 export const getAccessToken = () => {
+  console.log('Get access token: ', accessToken);
   return accessToken;
 };
